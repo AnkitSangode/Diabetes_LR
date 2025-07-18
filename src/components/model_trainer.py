f2bc4e599243a743d9c92056f443b46a187cbab0
Ankit <ankit.sangode@gmail.com>
2025-07-18 17:03:09 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,7 +39,57 @@ class ModelTrainer:
             "RandomForestClassifier": RandomForestClassifier()
             }
 
-            model_report :dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            param_grids = {
+    'LogisticRegression': [
+        {
+            'penalty': ['l1'],
+            'solver': ['liblinear', 'saga'],
+            'C': [0.01, 0.1, 1, 10],
+            'max_iter': [200, 500]
+        },
+        {
+            'penalty': ['l2'],
+            'solver': ['lbfgs', 'newton-cg', 'sag'],
+            'C': [0.01, 0.1, 1, 10],
+            'max_iter': [200, 500]
+        },
+        {
+            'penalty': ['elasticnet'],
+            'solver': ['saga'],
+            'C': [0.01, 0.1, 1, 10],
+            'l1_ratio': [0.1, 0.5, 0.9],
+            'max_iter': [200, 500]
+        }
+    ],
+    'AdaBoostClassifier': {
+        'n_estimators': [50, 100, 200, 500],
+        'learning_rate': [0.01, 0.1, 1, 10]
+    },
+    'KNeighborsClassifier': {
+        'n_neighbors': list(range(1, 31, 2)),
+        'weights': ['uniform', 'distance'],
+        'p': [1, 2]
+    },
+    'DecisionTreeClassifier': {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': list(range(2, 21)),
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': [None, 'sqrt', 'log2']
+    },
+    'RandomForestClassifier': {
+        'n_estimators': [100, 200, 400, 800],
+        'criterion': ['gini', 'entropy'],
+        # 'max_depth': [None, 5, 10, 20],
+        # 'min_samples_split': [2, 5, 10],
+        # 'min_samples_leaf': [1, 2, 4],
+        'max_features': [ 'sqrt', 'log2', None],
+        # 'bootstrap': [True, False]
+    }
+}
+
+
+            model_report :dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,params = param_grids)
 
             ## To get best model accuracy from dict
             best_model_accuracy = max(sorted(model_report.values()))
@@ -51,7 +101,7 @@ class ModelTrainer:
 
             best_model = models[best_model_name]
 
-            if best_model_accuracy < 0.6:
+            if best_model_accuracy < [0.6]:
                 raise CustomException('No model found with accuracy over 60%')
             
             logging.info('Best found model on both training and testing dataset')
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -21,14 +22,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,x_test,y_train,y_test,models):
+def evaluate_model(x_train,x_test,y_train,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3,n_jobs=-1)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
+            # model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
 
@@ -38,12 +46,12 @@ def evaluate_model(x_train,x_test,y_train,y_test,models):
 
             test_accuracy = accuracy_score(y_test,y_test_pred)
 
-            # Precision =  precision_score(y_test, y_test_pred)
-            # Recall = recall_score(y_test, y_test_pred)
-            # F1_Score =  f1_score(y_test, y_test_pred)
-            # Confusion_Matrix =  confusion_matrix(y_test, y_test_pred)
+            Precision =  precision_score(y_test, y_test_pred)
+            Recall = recall_score(y_test, y_test_pred)
+            F1_Score =  f1_score(y_test, y_test_pred)
+            Confusion_Matrix =  confusion_matrix(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_accuracy
+            report[list(models.keys())[i]] = [test_accuracy,train_accuracy,Precision,Recall,F1_Score,Confusion_Matrix]
 
         return report
     except Exception as e:
